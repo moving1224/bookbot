59b30828b6dc28965e2a138c6f910ad13cc397bb
moving1224 <moving1224@gmail.com>
2025-12-31 14:33:18 -0500

    CH2 L3&4 Complete

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+	# do something with f (the file) here
+	with open(path_to_file) as f:
+		# f is a file object
+		file_contents = f.read()
+	#print(file_contents)
+	return file_contents
+def wordcount(text):
+	words = text.split()
+	return words
+
+def main():
+	path_to_file = "./books/frankenstein.txt"
+	text = get_book_text(path_to_file)
+	words = wordcount(text)
+	print(f"Found {len(words)} total words")
+	
+main()
